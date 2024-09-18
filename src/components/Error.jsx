6b9efa1666d827d6c8d23349768765f33873be71
Akash Kundu <aakassh.uit@gmail.com>
2024-09-18 21:56:47 +0530

    update

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,12 +5,12 @@ const Error = () => {
   // console.log(err);
   return (
     <div className="flex items-center justify-center h-screen text-center">
-  <div>
-    <h1 className="text-4xl font-bold mb-4">Oops!!</h1>
-    <p className="text-xl">Something went wrong!</p>
-    <p className="text-lg text-gray-500 pt-2">{err.status} {err.statusText}</p>
-  </div>
-</div>
+      <div>
+        <h1 className="text-4xl font-bold mb-4">Oops!!</h1>
+        <p className="text-xl">Something went wrong!</p>
+        <p className="text-lg text-gray-500 pt-2">{err.status} {err.statusText}</p>
+      </div>
+    </div>
 
   )
 }
